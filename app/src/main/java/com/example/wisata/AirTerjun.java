43a6c14571e210cc6d8ea4be04aeee2eb73aed99
jstge <jstge28@gmail.com>
2022-04-19 20:13:50 +0700

    initial commit

---
+++ app/src/main/java/com/example/wisata/AirTerjun.java
@@ -0,0 +1,20 @@
+package com.example.wisata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AirTerjun extends AppCompatActivity {
+    Button btkembali;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_air_terjun);
+
+
+    }
+}
